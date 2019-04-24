b6e1137e1539608677a9bd058d9314f22aebd710
Diogo Ferrer <diogoramiresferrer@gmail.com>
2019-04-24 19:00:36 +0100

    Added ground and coin random spawn and excavation.

--- scripts.js
+++ scripts.js
@@ -17,22 +17,24 @@ var config = {
 };
 
 var player;
-var stars;
+var coins;
 var bombs;
 var platforms;
 var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
+var level = 1;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('sky', 'assets/sky.png');
+    this.load.image('sky', 'resources/sky.png');
+
+    // 16x16
     this.load.image('ground', 'resources/ground.png');
-    this.load.image('star', 'resources/coin.png');
-    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('coin', 'resources/coin.png');
     this.load.spritesheet('dude', 'resources/miner.png', { frameWidth: 100, frameHeight: 80 });
 }
 
@@ -44,21 +46,36 @@ function create ()
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
 
+    // The bitcoins to be collected
+    coins = this.physics.add.staticGroup();
+
     //  Here we create the ground.
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     for(var i = 0; i < 25; i++) {
-      for(var j = 0; j < 15; j++) {
-        platforms.create(16+i*32, 210+32*j, 'ground').setScale(2).refreshBody();
+      platforms.create(24+i*48, 224, 'ground').setScale(3).refreshBody();
+    }
+
+    for(var i = 0; i < 25; i++) {
+      for(var j = 1; j < 15; j++) {
+        if(Math.random() > 0.5) {
+          platforms.create(24+i*48, 224+48*j, 'ground').setScale(3).refreshBody();
+        } else {
+          if(Math.random() > 0.8) {
+            coins.create(24+i*48, 225+48*j, 'coin').setScale(0.8).refreshBody();
+          }
+        }
+
       }
     }
 
-    //  Now let's create some ledges
-    // platforms.create(600, 400, 'ground');
-    // platforms.create(50, 250, 'ground');
-    // platforms.create(750, 220, 'ground');
+
+    this.physics.world.setBounds(0, 0, 800, 1000, true, true, true, true);
 
     // The player and its settings
-    player = this.physics.add.sprite(100, 100, 'dude');
+    player = this.physics.add.sprite(400, 100, 'dude');
+    this.cameras.main.setBounds(0, 0, 400, 1000);
+    this.cameras.main.startFollow(player);
+    this.cameras.main.setBackgroundColor('#3d0c02');
 
     //  Player physics properties. Give the little guy a slight bounce.
     player.setBounce(0.2);
@@ -67,7 +84,7 @@ function create ()
     //  Our player animations, turning, walking left and walking right.
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 9 }),
+        frames: this.anims.generateFrameNumbers('dude', { coint: 0, end: 9 }),
         frameRate: 10,
         repeat: -1
     });
@@ -80,14 +97,14 @@ function create ()
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 9 }),
+        frames: this.anims.generateFrameNumbers('dude', { coint: 0, end: 9 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'dig',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 9 }),
+        frames: this.anims.generateFrameNumbers('dude', { coint: 0, end: 9 }),
         frameRate: 5,
         repeat: -1
     });
@@ -95,29 +112,27 @@ function create ()
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
+    //  Some coins to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    // coins = this.physics.add.group({
+    //     key: 'coin'
+    // });
 
-    stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    });
+    // coins.children.iterate(function (child) {
+    //
+    //     //  Give each coin a slightly different bounce
+    //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    //
+    // });
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  Collide the player and the stars with the platforms
+    //  Collide the player and the coins with the platforms
     this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
+    // this.physics.add.collider(coins, platforms);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(player, stars, collectStar, null, this);
+    //  Checks to see if the player overlaps with any of the coins, if he does call the collectcoin function
+    this.physics.add.overlap(player, coins, collectcoin, null, this);
 }
 
 function update ()
@@ -153,22 +168,33 @@ function update ()
 
     if (cursors.down.isDown) {
       player.anims.play('dig', true);
-      platforms.remove(platforms.children[0]);
+
+      platforms.children.iterate(function (child) {
+        try {
+          if(child.x >= player.x + 5 && child.x <= player.x + 50 && child.y <= player.y + 70 && child.y >= player.y) {
+            child.destroy();
+          }
+        } catch {
+          console.log("FIX ME");
+        }
+      });
     }
+
+    // console.log(player.y);
 }
 
-function collectStar (player, star)
+function collectcoin (player, coin)
 {
-    star.disableBody(true, true);
+    coin.disableBody(true, true);
 
     //  Add and update the score
     score += 10;
     scoreText.setText('Score: ' + score);
 
-    if (stars.countActive(true) === 0)
+    if (coins.countActive(true) === 0)
     {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
+        //  A new batch of coins to collect
+        coins.children.iterate(function (child) {
 
             child.enableBody(true, child.x, 0, true, true);
 
