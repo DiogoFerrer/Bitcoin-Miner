5dff83b47e50aae0f633df61ee7327ddf942e2e1
Diogo Ferrer <diogoramiresferrer@gmail.com>
2019-05-12 18:45:57 +0100

    Highscores almost finished. Added ground animation

--- src/Highscores.js
+++ src/Highscores.js
@@ -5,20 +5,37 @@ class Highscores extends Phaser.Scene {
 
   preload() {
     this.load.json('highscores', './resources/highscores.json');
+    this.load.image('highBg', './resources/highBg.png');
+  }
+
+  getPadding(len) {
+    var result = "";
+    for(var i = len; i < 7; i++) {
+      result += " ";
+    }
+    return result;
   }
 
   create() {
     // Fetch the highscores
     this.highscores = this.cache.json.get('highscores')['highscores'];
 
-    for(var i = 0; i < this.highscores.length; i++) {
-      this.add.text(250, 200+i*100, this.highscores[i]["name"] + " " + this.highscores[i]["score"], { fontSize: "32px", fill: "#fff" });
+    // Add a background
+    this.add.image(400, 300, 'highBg');
 
-      console.log(this.highscores[i]);
+    // Display highscores
+    for(var i = 0; i < this.highscores.length; i++) {
+      this.add.text(200, 200+i*100, i+1 + ". " + this.highscores[i]["name"] + this.getPadding(this.highscores[i]["name"].length) + " - Level " + this.highscores[i]["score"], { fontSize: "32px", fill: "#000" });
     }
+
+    this.add.text(16, 575, 'Press ESC to leave', { fontFamily: 'Monospace', fill: '#000' });
+    this.exitButton = this.input.keyboard.addKey('ESC');
   }
 
   update() {
-
+    if (this.exitButton.isDown) {
+      this.scene.launch('Main');
+      this.scene.stop();
+    }
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,8 @@ class Main extends Phaser.Scene {
   preload() {
     this.load.image('startButton', './resources/startButton.png');
     this.load.image('sky', 'resources/sky.png');
-    this.load.image('ground', 'resources/ground.png');
+    this.load.spritesheet('ground', 'resources/ground.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.image('cup', 'resources/cup.png');
   }
 
   create() {
@@ -16,18 +17,29 @@ class Main extends Phaser.Scene {
         this.add.image(24+i*48, 224+48*j, 'ground').setScale(3);
       }
     }
+
     this.add.text(150, 80, 'Bitcoin Miner', { fontFamily: 'Monospace', fontSize: '64px', fill: '#000' });
+
     this.startButton = this.add.image(390, 320, 'startButton').setScale(0.2).setInteractive();
+
+    this.scoresButton = this.add.image(100, 525, 'cup').setScale(0.2).setInteractive();
+    this.add.text(55, 555, 'Highscores', { fontFamily: 'Monospace', fontSize: '16px', fill: '#ffff00' });
 
     this.option = 0;
     this.startButton.on('pointerdown', function(ev) {
       this.option = 1;
+    }, this);
+    this.scoresButton.on('pointerdown', function(ev) {
+      this.option = 2;
     }, this);
   }
 
   update() {
     if(this.option === 1) {
       this.scene.start('Game');
+    }
+    else if(this.option === 2) {
+      this.scene.start('Highscores');
     }
   }
 };
